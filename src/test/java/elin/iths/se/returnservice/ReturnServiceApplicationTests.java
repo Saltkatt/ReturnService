f3f7da4542535c82f3ad4d972f3900fcc633a0df
Elin <elin.sexton@iths.se>
2019-03-17 16:32:52 +0100

    Initial commit

---
+++ src/test/java/elin/iths/se/returnservice/ReturnServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package elin.iths.se.returnservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReturnServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
